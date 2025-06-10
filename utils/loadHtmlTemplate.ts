fdf27641b183d364044e3aa038e4dec767ff365e
Quang Le Nhat <quang07112004@gmail.com>
2025-06-11 00:45:28 +0700

    feat/ [JW-0] email template

---
+++ utils/loadHtmlTemplate.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import handlebars from 'handlebars';
+
+export const loadHtmlTemplate = (
+    templateName: string,
+    values: Record<string, unknown>
+): string => {
+    const filePath = path.join(process.cwd(), 'src', 'templates', `${templateName}.html`);
+    const source = fs.readFileSync(filePath, 'utf8');
+    const template = handlebars.compile(source);
+    return template(values);
+};
