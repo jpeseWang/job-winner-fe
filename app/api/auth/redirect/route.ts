1fb44b0e4f0252ceca4bebd0e5748cc77b2cf481
Quang Le Nhat <quang07112004@gmail.com>
2025-06-11 22:28:26 +0700

    fix/ NextRequest type

--- app/api/auth/redirect/route.ts
+++ app/api/auth/redirect/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 import { getServerSession } from "next-auth"
 import { authOptions } from "../[...nextauth]/route"
 import { UserRole } from "@/types/enums"
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const session = await getServerSession(authOptions)
 
   if (!session || !session.user?.role) {
