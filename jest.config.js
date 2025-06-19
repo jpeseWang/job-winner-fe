f0167628d3fda4587acfe700d5b9647e74ef6ebe
Le Nhat Quang <118867476+jpeseWang@users.noreply.github.com>
2025-06-19 09:47:56 +0000

    [JW-0] Unit test setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',    // <== Add this
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
